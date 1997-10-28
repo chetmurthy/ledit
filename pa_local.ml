679cf0fc7282daa13aebad3d3c4dea5574e2e58f
Daniel de Rauglaudre <daniel.roglo@free.fr>
1997-10-28 10:01:04 +0000

    -

--- pa_local.ml
+++ pa_local.ml
@@ -1,3 +1,14 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                               Ledit                                 *)
+(*                                                                     *)
+(*       Daniel de Rauglaudre, projet Cristal, INRIA Rocquencourt      *)
+(*                                                                     *)
+(*  Copyright 1997 Institut National de Recherche en Informatique et   *)
+(*  Automatique.  Distributed only by permission.                      *)
+(*                                                                     *)
+(***********************************************************************)
+
 (* $Id$ *)
 
 open Pcaml;
