35c4bc29b277c6cc10cd44dbbe4908b01085660e
Daniel de Rauglaudre <daniel.roglo@free.fr>
2001-06-16 14:03:43 +0000

    -

--- pr_local.ml
+++ pr_local.ml
@@ -11,6 +11,7 @@ value loc = (0, 0);
 
 value expr e dg k = pr_expr.pr_fun "top" e dg k;
 value patt e dg k = pr_patt.pr_fun "top" e dg k;
+value expr_fun_args ge = Extfun.apply pr_expr_fun_args.val ge;
 
 value is_local_def p pel1 pel2 e =
   try
@@ -42,11 +43,42 @@ value is_local_def p pel1 pel2 e =
   [ Not_found -> False ]
 ;
 
+value rec list elem =
+  fun
+  [ [] -> fun _ k -> k
+  | [x] -> fun dg k -> [: `elem x dg k :]
+  | [x :: l] -> fun dg k -> [: `elem x "" [: :]; list elem l dg k :] ]
+;
+
+value rec listwbws elem b sep el k =
+  match el with
+  [ [] -> [: b; k :]
+  | [x] -> [: `elem b x k :]
+  | [x :: l] -> [: `elem b x [: :]; listwbws elem [: `sep :] sep l k :] ]
+;
+
+value rec bind_list b pel k =
+  match pel with
+  [ [pe] -> let_binding b pe k
+  | pel ->
+      Vbox [: `HVbox [: :]; listwbws let_binding b (S LR "and") pel k :] ]
+and let_binding b (p, e) k =
+  BEbox [: let_binding0 [: b; `patt p "" [: :] :] e [: :]; k :]
+and let_binding0 b e k =
+  let (pl, e) = expr_fun_args e in
+  [: `HVbox [: `HVbox b; `HOVbox (list patt pl "" [: `S LR "=" :]) :];
+     `expr e "" k :]
+;
+
 let lev = find_pr_level "top" pr_str_item.pr_levels in
 lev.pr_rules :=
   extfun lev.pr_rules with
-  [ <:str_item< value $p$ = let $list:pel1$ in let $list:pel2$ in $e$ >>
-    when is_local_def p pel1 pel2 e ->
+  [ <:str_item< value $p$ = let $p1$ = $e1$ in let $list:pel2$ in $e$ >>
+    when is_local_def p [(p1, e1)] pel2 e ->
       fun curr next _ k ->
-        [: curr <:str_item< let $list:pel1$ in let $list:pel2$ in () >> ""
-             k :] ];
+        let pel1 = [(p1, e1)] in
+        let r = [: :] in
+        [: `Vbox
+             [: `HVbox [: :];
+                `bind_list [: `S LR "local"; r :] pel1 [: `S LR "in" :];
+                curr <:str_item< value $list:pel2$ >> "" k :] :] ];
